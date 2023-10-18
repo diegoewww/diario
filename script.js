579d0d5bfb850f6a5ee1794fc243923ebec9554c
tuvieja <105611148+n1na666@users.noreply.github.com>
2023-10-18 12:18:40 -0500

    fang

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+var letterCombinations = function(digits) {
+
+  if(digits.length === 0){
+      return []
+  }
+
+  const phone_number = ["abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"]
+  const output = []
+  backtrack("", digits,phone_number,output)
+  return output;
+
+  function backtrack(conbination, next_digits , phone_number,output){
+      if (next_digits.length === 0) {
+          output.push(conbination);
+      }else{
+          const leters =  phone_number[next_digits[0] - 2]
+          for (const leter of leters){
+              backtrack(conbination+leter,next_digits.slice(1),phone_number,output)
+          }
+
+      }
+  }
+}
 
 Array.prototype.myMap = function (cb) {
   let temp = []
@@ -95,24 +118,8 @@ const totalMarks = students.map(student =>{
 }).filter(student => student.marks >60)
   .reduce((acc,student)=> acc + student.marks,0)
 
-console.log(totalMarks);
+// pREPARACION PARA FAANG AHORA!
 
-// console.log(namesfiltered)
-
-// console.log(sumOfMarks)
-
-// for (let i = 0; i < moreThan60Marks.length; i++) {
-//   console.log(moreThan60Marks[i].name)
-// }
-// const onlyNames = moreThan60Marks.map(element=>{
-//   return element.name
-// })
-// console.log(onlyNames)
-// console.log(moreThan60Marks)
-
-// console.log(mapNames)
-
-// console.log(addName())
 
 
 
