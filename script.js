76be3e0cc81026138f1d456cbaa07d9ff2e12a66
tuvieja <105611148+n1na666@users.noreply.github.com>
2023-10-24 16:43:44 -0500

    isDuplicate

--- script.js
+++ script.js
@@ -449,9 +449,9 @@ class TimeLimitedCache {
   }
 }
 
-// blind 75
+// blind 75 
 
-const isDuplicate = function (nums) {
+const isDuplicate1 = function (nums) {
   for (let i = 0; i < nums.length; i++) {
     for(let j = i+1 ; j< nums.length; j++){
       if(nums[i] === nums[j]){
@@ -461,6 +461,49 @@ const isDuplicate = function (nums) {
   }
   return false
 }
+
+const isDuplicate2 = function (nums) {
+  
+  nums.sort(function(a, b){return a - b})
+
+  for(let i = 0; i<nums.length-1; i++){
+    if(nums[i] === nums[i+1]){
+      return true
+    }
+  }
+  return false
+
+}
+
+const isDuplicate3 = function (nums) {
+
+  let numsSet = new Set(nums)
+  let equal = numsSet.size === nums.length
+  return !equal
+  
+}
+
+const isDuplicate4 = function (nums, numsSet = new Set()) {
+  for(const num of nums){
+    if(numsSet.has(num)) return true
+    numsSet.add(num)
+  }
+  return false
+}
+
+const isDuplicate5 = function (nums) {
+  let cache = {}
+  for (let i = 0; i < nums.length; i++) {
+    if(cache[nums[i]] === nums[i]){
+      return true
+    }
+    cache[nums[i]] = nums[i]
+  }
+  return false
+  
+}
+
+console.log(isDuplicate5([1,2,3,1]));
 
 
         
