b6b9d5ff5d1feceb51addde6b79628c4aabe6951
tuvieja <105611148+n1na666@users.noreply.github.com>
2023-10-30 17:31:26 -0500

    maxSub

--- arrp.js
+++ arrp.js
@@ -162,3 +162,35 @@ function largestSum(nums) {
 // Input: [-2,1,-3,4,-1,2,1,-5,4]     ----->>>>>    Output: 6,   [4,-1,2,1]
 // Input: [5,4,-1,7,8]                ----->>>>>    Output: 23,  [5,4,-1,7,8]
 
+function maxSubarray(nums) {
+    let sum = 0
+    let max = nums[0]
+    for (let i = 0; i < nums.length; i++) {
+         sum = sum + nums[i]
+         if(sum > max){
+            max = sum
+         }
+         if(sum < 0){
+            sum = 0
+         }
+    }
+    return max
+    
+}
+
+function maxSubarray2(nums) {
+
+    let max = nums[0]
+
+    for (let i = 0; i < nums.length; i++) {
+        let currentSum = 0;
+        for (let j = 0; j < array.length; j++) {
+          currentSum = currentSum + nums[j]
+          if(currentSum > max){
+            max = currentSum
+          } 
+        }   
+    }
+    
+}
+console.log(maxSubarray([5,4,-1,7,8]));
